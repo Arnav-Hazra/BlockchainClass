17785f77c56d6564c9dd5a845d1bc81f9b35b04d
Arnav-Hazra <54741339+Arnav-Hazra@users.noreply.github.com>
2020-09-17 22:04:53 +0530

    Update StudentInfo.sol
    v2

--- StudentInfo.sol
+++ StudentInfo.sol
@@ -2,35 +2,22 @@ pragma solidity ^0.5.17;
 contract StudentInfo {
     struct SInfo { 
       string Name;
-      uint num;
+      uint RegNum;
       string gender;
-      uint enroll;
+      uint enrollYr;
     }
-    SInfo sinfo;
+    SInfo public sinfo;
     
    function setName(string memory _msg) public {
       sinfo.Name = _msg;
    }
    function setRegNum(uint _msg) public {
-      sinfo.num = _msg;
+      sinfo.RegNum = _msg;
    }
    function setGender(string memory _msg) public {
       sinfo.gender = _msg;
    }
    function setEnrollYr(uint _msg) public {
-      sinfo.enroll = _msg;
-   }
-   
-   function getName() public view returns (string memory) {
-      return sinfo.Name;
-   }
-   function getRegNum() public view returns (uint) {
-      return sinfo.num;
-   }
-   function getGender() public view returns (string memory) {
-      return sinfo.gender;
-   }
-   function getEnrollYr() public view returns (uint){
-      return sinfo.enroll;
+      sinfo.enrollYr = _msg;
    }
 }
